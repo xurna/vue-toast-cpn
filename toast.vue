0d35754f6011c844e28282ba4a04858b6976d927
yangxiaona <yangxiaona@qiushibaike.com>
2018-06-08 17:15:50 +0800

    opt:init cpn

---
+++ toast.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div v-show="show" class="toastContainer" :style="style">
+      <div class="toastText">{{text}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: false,
+      text: '',
+      type: 'center',
+      style: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.toastContainer {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  position: fixed;
+  left: 0;
+  right: 0;
+  width: 100%;
+  z-index: 9999;
+  text-align: center;
+}
+
+.toastText {
+  display: inline-block;
+  max-width: 80%;
+  background: rgba(0, 0, 0, 0.8);
+  padding: 12px 22px;
+  text-align: center;
+  font-size: 15px;
+  line-height: 22px;
+  border-radius: 5px;
+  color: #fff;
+}
+
+.hide {
+  display: none;
+}
+
+.show {
+  display: block;
+}
+</style>
